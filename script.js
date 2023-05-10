559e4822f39c1f0820b3b4f2b11032fda669f42a
Shota Pailodze <55694002+shotapailodze@users.noreply.github.com>
2023-05-10 13:51:07 +0400

    countdown function

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const inputContainer = document.getElementById('input-container');
-const countdownForm = document.getElementById('countdownForm');
+const countdownForm = document.getElementById('countdownform');
 const dateEl = document.getElementById('date-picker');
+
+let countdownTitle = '';
+let countdownDate = '';
 
 // Set Date Input Min With Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// Take Values from Form Input
+function updateCountdown(e) {
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+    console.log(countdownDate, countdownTitle);
+}
+
+// Event Listener
+countdownForm.addEventListener('submit',updateCountdown);
